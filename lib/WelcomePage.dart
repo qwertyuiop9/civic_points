74ab09311be60909e08800cde0aeccddd8f377cf
hike86 <48754252+hike86@users.noreply.github.com>
2021-03-03 23:02:24 +0100

    SingleChildScrollView aggiunto nel WelcomePage.dart

--- lib/WelcomePage.dart
+++ lib/WelcomePage.dart
@@ -10,88 +10,90 @@ class WelcomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
       ),
-      body: Container(
-        margin: const EdgeInsets.fromLTRB(16, 16, 0, 0),
-        width: double.infinity,
-        color: Colors.white,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: NetworkImage(
-                  imageUrl,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(16, 16, 0, 0),
+          width: double.infinity,
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    imageUrl,
+                  ),
+                  radius: 60,
+                  backgroundColor: Colors.transparent,
                 ),
-                radius: 60,
-                backgroundColor: Colors.transparent,
-              ),
-              SizedBox(height: 40),
-              Text(
-                'Utente',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                name,
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.deepPurple,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'E-mail',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                email,
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.deepPurple,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 40),
-              RaisedButton(
-                onPressed: () {
-                  signOutGoogle();
-                  Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) {return LoginPage();}), ModalRoute.withName('/'));
-                },
-                child: Text("Sign Out"),
-                textColor: Colors.white,
-                color: Colors.blueGrey,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+                SizedBox(height: 40),
+                Text(
+                  'Utente',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
-                elevation: 5,
-              ),
-              SizedBox(height: 40),
-              RaisedButton(
-                onPressed: () {
-                  Navigator.push(
-                      context, MaterialPageRoute(builder: (context) => createEvent()));
-                },
-                child: Text("Crea evento"),
-                textColor: Colors.white,
-                color: Colors.blueGrey,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+                Text(
+                  name,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.deepPurple,
+                      fontWeight: FontWeight.bold),
                 ),
-                elevation: 5,
-              ),
-              SizedBox(height: 40),
-              RaisedButton(
-                onPressed: () {
-                  navigateToEventListPage(context);
-                },
-                child: Text("Eventi disponibili"),
-                textColor: Colors.white,
-                color: Colors.blueGrey,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+                SizedBox(height: 20),
+                Text(
+                  'E-mail',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 ),
-                elevation: 5,
-              )
-            ],
+                Text(
+                  email,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.deepPurple,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 40),
+                RaisedButton(
+                  onPressed: () {
+                    signOutGoogle();
+                    Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) {return LoginPage();}), ModalRoute.withName('/'));
+                  },
+                  child: Text("Sign Out"),
+                  textColor: Colors.white,
+                  color: Colors.blueGrey,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  elevation: 5,
+                ),
+                SizedBox(height: 40),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context, MaterialPageRoute(builder: (context) => createEvent()));
+                  },
+                  child: Text("Crea evento"),
+                  textColor: Colors.white,
+                  color: Colors.blueGrey,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  elevation: 5,
+                ),
+                SizedBox(height: 40),
+                RaisedButton(
+                  onPressed: () {
+                    navigateToEventListPage(context);
+                  },
+                  child: Text("Eventi disponibili"),
+                  textColor: Colors.white,
+                  color: Colors.blueGrey,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  elevation: 5,
+                )
+              ],
+            ),
           ),
         ),
       ),
